e90249c09a4292f0c12f0182724fd518a1466429
octavia <Ogomes21@gmail.com>
2022-08-06 22:11:06 -0400

    Finished! But, my message won't post to the thread.

--- message-Service.js
+++ message-Service.js
@@ -1,24 +1,56 @@
 export default class MessageService {
 
     getAllMessages() {
-        let request = new XMLHttpRequest();
+        const request = new XMLHttpRequest();
 
-    new Promise(function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
         // Setup our listener to process completed requests
         request.onload = function() {
             // Process the response
             if (request.status >= 200 && request.status < 300) {
-                const threads = JSON.parse(request.responseText); // 'This is the returned text.'
+            // If successful
+                const threads = JSON.parse(request.responseText);
+                // 'This data is passed to the success callback
                 resolve(threads);
             } else {
-                console.log('Error: ' + request.status); // An error occurred during the request.
+                // this data is passed to the failure callback
+                reject({
+                    status: request.status,
+                    statusText: request.statusText
+                });
             }
-        // do something
         };
-
+        // do something
         request.open("GET", "http://zipcode.rocks:8085/messages");
 
         request.send();
+
+        })
+    }
+
+    createNewMessage(message) {
+    const request = new XMLHttpRequest();
+
+    return new Promise(function (resolve, reject) {
+    // Setup our listener to process complete requests
+    request.onload = function () {
+    // Process the response
+    if (request.status >= 200 && request.status < 300) {
+    // If successful
+    resolve(JSON.parse(request.responseText));
+     } else {
+        reject({
+            status: request.status,
+            statusText: request.statusText
         });
-}
+       }
+     };
+
+     request.open("POST", `http://zipcode.rocks:8085/ids/${message.fromid}/messages`);
+
+     request.send(JSON.stringify(message));
+
+     });
+ }
+
 }
